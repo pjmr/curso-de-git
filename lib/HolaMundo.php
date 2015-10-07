c0e18ef54c1a05882e45c2e4c9594b85a0adae29
Pedro Muñoz <dexis@outlook.com>
2015-10-07 17:45:44 +0200

    Añadida la clase HolaMundo

---
+++ lib/HolaMundo.php
@@ -0,0 +1,15 @@
+<?php
+	// Autor: Pedro
+
+  class HolaMundo
+  {
+     private $nombre;
+     function __construct($nombre)
+     {
+     	$this->nombre = $nombre;
+     }
+     function __toString()
+     {
+        return sprintf ("Hola, %s.\n", $this->nombre);
+     }
+}
