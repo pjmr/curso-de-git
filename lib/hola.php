8d7340f4fa7b86aa0c5ea20e440552ee7a9d2f90
Pedro Muñoz <dexis@outlook.com>
2015-10-07 18:26:46 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
-// Autor: Pedro
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola {$nombre\n";
+  // Autor: Sergio Gómez <sergio@uco.es>
+  print "Introduce tu nombre:";
+  $nombre = trim(fgets(STDIN));
+  @print "Hola, {$nombre}\n";
